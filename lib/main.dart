d8bea87c2ac5401c6a233bffbfc50b40b4307eb7
Nini Gedesguri <nini@Ninis-iMac.local>
2025-03-25 13:41:16 +0400

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gym_app/screens/welcome_view.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: WelcomeView(),
     );
   }
 }
---
+++ lib/screens/welcome_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class WelcomeView extends StatefulWidget {
+  const WelcomeView({super.key});
+
+  @override
+  State<WelcomeView> createState() => _WelcomeViewState();
+}
+
+class _WelcomeViewState extends State<WelcomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/image1.png"), fit: BoxFit.cover)),
+      ),
+    );
+  }
+}

fc8394341fe64f61d45437219b1370ff7c273be5
Nini Gedesguri <nini@Ninis-iMac.local>
2025-03-25 14:31:37 +0400

    add

--- lib/screens/welcome_view.dart
+++ lib/screens/welcome_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WelcomeView extends StatefulWidget {
   const WelcomeView({super.key});
@@ -12,9 +13,60 @@ class _WelcomeViewState extends State<WelcomeView> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        height: double.infinity,
+        width: double.infinity,
         decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/image1.png"), fit: BoxFit.cover)),
+          image: DecorationImage(
+            image: AssetImage("assets/image1.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 70),
+                  child: Text(
+                    "HARD ",
+                    style: GoogleFonts.bebasNeue(
+                        fontSize: 32, color: Colors.white, letterSpacing: 1.2),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 70),
+                  child: Text(
+                    "Way",
+                    style: GoogleFonts.bebasNeue(
+                        fontSize: 32,
+                        color: Color(0xFF40D876),
+                        letterSpacing: 1.2),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "About You,",
+                  style: GoogleFonts.lato(
+                      fontSize: 42,
+                      fontWeight: FontWeight.w800,
+                      color: Colors.white),
+                ),
+                Text(
+                  "we want to know more about you, follow the next steps\n to complete the information",
+                  style: GoogleFonts.lato(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
